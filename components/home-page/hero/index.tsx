5f83771ae0cbc305e827bc7db875c6b717724100
猫河 <catsriver@163.com>
2022-10-07 13:20:37 +0800

    feat: 添加Hero组件

--- components/home-page/hero/index.tsx
+++ components/home-page/hero/index.tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image'
 import { FC } from 'react'
 
 import styles from './styles.module.css'
 
 const Hero: FC = () => {
-    return <></>
+    return (
+        <section className={styles.hero}>
+            <div className={styles.image}>
+                <Image
+                    src='/images/site/xiaoxin.jpeg'
+                    alt='An image showing 小新'
+                    width={300}
+                    height={300}
+                />
+            </div>
+            <h1>Hi, I&apos;m 野原新之助</h1>
+            <p>
+                I blog about web development - especially frontend frameworks
+                like Angular or React.
+            </p>
+        </section>
+    )
 }
 
 export default Hero
