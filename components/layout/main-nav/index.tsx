ceb6ff58d68f4fc501c7cfdefd43ef02ee0a69b4
猫河 <catsriver@163.com>
2022-10-07 13:41:53 +0800

    feat: 添加导航

--- components/layout/main-nav/index.tsx
+++ components/layout/main-nav/index.tsx
@@ -1,7 +1,28 @@
+import Link from 'next/link'
 import { FC } from 'react'
 
+import Logo from '../logo'
+
 const MainNav: FC = () => {
-    return <></>
+    return (
+        <header>
+            <Link href='/'>
+                <a>
+                    <Logo />
+                </a>
+            </Link>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href='/posts'>Posts</Link>
+                    </li>
+                    <li>
+                        <Link href='/contact'>Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
 export default MainNav
