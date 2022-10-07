8569134662b309835e64abe21890aa8ea311dee6
猫河 <catsriver@163.com>
2022-10-07 14:27:52 +0800

    feat: 渲染虚拟Post数据

--- components/posts/post-item/index.tsx
+++ components/posts/post-item/index.tsx
@@ -1,9 +1,53 @@
 import { FC } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 import styles from './styles.module.css'
 
-const PostItem: FC = () => {
-    return <></>
+interface PostType {
+    title: string
+    image: string
+    excerpt: string
+    date: Date
+    slug: string
+}
+
+interface PostItemProps {
+    post: PostType
+}
+
+const PostItem: FC<PostItemProps> = ({ post }) => {
+    const { title, image, excerpt, date, slug } = post
+
+    const formattedDate = new Date(date).toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric'
+    })
+
+    const imagePath = `/images/posts/${slug}/${image}`
+
+    return (
+        <li className={styles.post}>
+            <Link href={''}>
+                <a>
+                    <div className={styles.image}>
+                        <Image
+                            src={imagePath}
+                            alt={title}
+                            width={300}
+                            height={200}
+                        />
+                    </div>
+                    <div className={styles.content}>
+                        <h3>{title}</h3>
+                        <time>{formattedDate}</time>
+                        <p>{excerpt}</p>
+                    </div>
+                </a>
+            </Link>
+        </li>
+    )
 }
 
 export default PostItem
