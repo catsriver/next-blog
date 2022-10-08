f3d3dad034475b202ce35caa0aa1190a8e7b1f05
猫河 <catsriver@163.com>
2022-10-08 12:00:51 +0800

    feat: 准备Contact表单

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,9 @@
 import { NextPage } from 'next/types'
 
+import ContactForm from '../components/contact-form'
+
 const ContactPage: NextPage = () => {
-    return <div>Contact Page</div>
+    return <ContactForm />
 }
 
 export default ContactPage
