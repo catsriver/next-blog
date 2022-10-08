53e5392d539d74e16637ad64b7a8e5911a1ae5c3
猫河 <catsriver@163.com>
2022-10-08 16:09:01 +0800

    feat: 为不同环境设置env变量

--- next.config.js
+++ next.config.js
@@ -1,12 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    env: {
-        mongodb_username: 'catsriver',
-        mongodb_password: 'zhenxi',
-        mongodb_clustername: 'cluster0',
-        mongodb_database: 'my-site'
+
+const { PHASE_DEVELOPMENT_SERVER } = require('next/dist/shared/lib/constants')
+
+const nextConfig = (phase) => {
+    const commonConfig = {
+        reactStrictMode: true,
+        swcMinify: true
+    }
+
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return {
+            ...commonConfig,
+            // 为开发环境设置环境变量
+            env: {
+                mongodb_username: 'catsriver',
+                mongodb_password: 'zhenxi',
+                mongodb_clustername: 'cluster0',
+                mongodb_database: 'my-site-dev'
+            }
+        }
+    }
+
+    return {
+        ...commonConfig,
+        // 为生产环境设置环境变量
+        env: {
+            mongodb_username: 'catsriver',
+            mongodb_password: 'zhenxi',
+            mongodb_clustername: 'cluster0',
+            mongodb_database: 'my-site'
+        }
     }
 }
 
