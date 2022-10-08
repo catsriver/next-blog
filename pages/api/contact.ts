2702a2391dcda6b5c9de6d4175420836dbecfb01
猫河 <catsriver@163.com>
2022-10-08 13:39:56 +0800

    feat: 使用mongodb数据库存储消息

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,3 +1,4 @@
+import { MongoClient } from 'mongodb'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export interface BodyEnterty {
@@ -10,7 +11,7 @@ interface ReqApiProps extends NextApiRequest {
     body: BodyEnterty
 }
 
-const hander = (req: ReqApiProps, res: NextApiResponse) => {
+const hander = async (req: ReqApiProps, res: NextApiResponse) => {
     if (req.method === 'POST') {
         const { email, message, name } = req.body
 
@@ -27,9 +28,31 @@ const hander = (req: ReqApiProps, res: NextApiResponse) => {
         }
 
         // Store it in a database
-        const newMessage = { email, name, message }
+        const newMessage: { [key: string]: any } = { email, name, message }
 
-        console.log(newMessage)
+        let client
+
+        try {
+            client = await MongoClient.connect(
+                'mongodb+srv://catsriver:zhenxi@cluster0.o61fatb.mongodb.net/?retryWrites=true&w=majority'
+            )
+        } catch (err) {
+            res.status(500).json({ message: 'Could not connect to database.' })
+            return
+        }
+
+        const db = client.db('my-site')
+
+        try {
+            const result = await db.collection('messages').insertOne(newMessage)
+            newMessage.id = result.insertedId
+        } catch (err) {
+            client.close()
+            res.status(500).json({ message: 'Storing message failed.' })
+            return
+        }
+
+        client.close()
 
         res.status(201).json({
             message: 'Successfully stored message!',
