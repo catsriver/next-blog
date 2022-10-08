7e9c14b8f9f938407d0e7696b863e512b396be3d
猫河 <catsriver@163.com>
2022-10-08 10:05:05 +0800

    feat: 替换img为Image

--- components/posts/post-detail/post-content/index.tsx
+++ components/posts/post-detail/post-content/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import ReactMarkdown from 'react-markdown'
+import Image from 'next/image'
 
 import PostHeader from '../post-header/index'
 import styles from './styles.module.css'
@@ -8,10 +9,26 @@ import { PostType } from '../../post-item/index'
 const PostContent: FC<{ post: PostType }> = ({ post }) => {
     const imagePath = `/images/posts/${post.slug}/${post.image}`
 
+    const customComponents = {
+        // 替换img标签为Image
+        img(img: any) {
+            return (
+                <Image
+                    src={`/images/posts/${post.slug}/${img.src}`}
+                    alt={img.alt}
+                    width={600}
+                    height={300}
+                />
+            )
+        }
+    }
+
     return (
         <article className={styles.content}>
             <PostHeader image={imagePath} title={post.title} />
-            <ReactMarkdown>{post.content}</ReactMarkdown>
+            <ReactMarkdown components={customComponents}>
+                {post.content}
+            </ReactMarkdown>
         </article>
     )
 }
